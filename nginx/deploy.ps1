9f21c77e953cdc61a3f1b2a1c8ab3beb1b954d06
Swagata Prateek <thehoneymad@hotmail.com>
2017-02-24 19:50:32 +0100

    fixed deploy script

--- nginx/deploy.ps1
+++ nginx/deploy.ps1
@@ -1,1 +1,12 @@
 Copy-Item -force $PSScriptRoot\nginx.conf c:\nginx\conf\
+
+$nginx = Get-Process nginx -ErrorAction SilentlyContinue
+if ($nginx) {
+    & "C:\nginx\nginx.exe -s reload"
+    # wait five seconds
+    Sleep 5
+}
+else {
+    & "start C:\nginx\nginx.exe"
+}
+Remove-Variable nginx
