d881ab9e6d7d1b7edcf3a5c71927ad09ca1f6a52
Swagata Prateek <thehoneymad@hotmail.com>
2017-02-24 19:57:34 +0100

    Added more changes to reload the config

--- nginx/deploy.ps1
+++ nginx/deploy.ps1
@@ -2,11 +2,11 @@ Copy-Item -force $PSScriptRoot\nginx.conf c:\nginx\conf\
 
 $nginx = Get-Process nginx -ErrorAction SilentlyContinue
 if ($nginx) {
-    & "C:\nginx\nginx.exe -s reload"
+    & 'C:\nginx\nginx.exe' '-s reload'
     # wait five seconds
     Sleep 5
 }
 else {
-    & "start C:\nginx\nginx.exe"
+    & 'start C:\nginx\nginx.exe'
 }
 Remove-Variable nginx
