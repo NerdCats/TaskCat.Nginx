a1020034675be441bdfee5f44010c0733290ff7a
Swagata Prateek <thehoneymad@hotmail.com>
2017-02-24 20:36:24 +0100

    Trying to use a bat file now

--- nginx/deploy.ps1
+++ nginx/deploy.ps1
@@ -1,18 +1,16 @@
 Copy-Item -force $PSScriptRoot\nginx.conf c:\nginx\conf\
+Copy-Item -force $PSScriptRoot\*.bat c:\nginx\
 
 $nginx = Get-Process nginx -ErrorAction SilentlyContinue
 if ($nginx) {
-    $CMD = 'C:\nginx\nginx.exe'
-    $arg1 = '-s'
-    $arg2 = 'reload'
-    & $CMD $arg1 $arg2
+    $CMD = 'C:\nginx\reload.bat'
+    start-process $CMD
     # wait five seconds
     Sleep 5
 }
 else {
-    $CMD = 'start'
-    $arg1 = 'C:\nginx\nginx.exe'
-    & $CMD $arg1
+    $CMD = 'C:\nginx\start.bat'
+    start-process $CMD
 }
 
 Remove-Variable nginx
